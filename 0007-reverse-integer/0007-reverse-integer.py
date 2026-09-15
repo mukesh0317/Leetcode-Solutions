95015264d6e946363ebe3659f0a65365b17bffd4
MUKESH KUMAR S R <mukeshkumar.sr17@gmail.com>
2026-09-15 09:37:32 +0530

    Time: 38 ms (91.8%), Space: 19.3 MB (9.09%) - LeetHub - Time: O(log(x)), Space: O(1)

---
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -0,0 +1,17 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        sign = -1 if x < 0 else 1
+        x = abs(x)
+        rev = 0
+        while x:
+            rev = rev * 10 + x % 10
+            x //= 10
+        rev *= sign
+        if -2**31 <= rev <= 2**31 - 1 :
+            return rev
+        else:
+            return 0
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
